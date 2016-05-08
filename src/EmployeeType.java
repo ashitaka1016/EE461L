2a005af05de0e6d878fcdf1e2b02168740457a9d
ashitaka1016 <Aaron@Aarons-MacBook-Pro.local>
2016-05-08 18:24:11 -0500

    deleted the junk

--- src/EmployeeType.java
+++
@@ -1,6 +0,0 @@
-
-public interface EmployeeType {
-	public double calcContribution(Employee e);
-	public double calcCompensation(Employee e);
-	public int analyzeProductivity(double cont, double comp);
-}
